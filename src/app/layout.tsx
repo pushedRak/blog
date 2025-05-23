b86abad1182f2dfc1d0f0f1751e0d03d7257756d
Minrak Son <dungguk@naver.com>
2025-05-24 00:43:42 +0900

    git init: Next.js 프로젝트 생성

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "락 블로그",
+  description: "개발자 손민락의 블로그입니다.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
