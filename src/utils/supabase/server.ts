b6c9e5f19333375b97566e92697de33fd59619e6
Minrak Son <dungguk@naver.com>
2025-05-24 23:54:21 +0900

    feat: 클라이언트용 supabase client 정의

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,7 +1,9 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+
 export async function createClient() {
   const cookieStore = await cookies();
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -16,9 +18,7 @@ export async function createClient() {
               cookieStore.set(name, value, options)
             );
           } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            // Server Component에서 호출된 경우 무시
           }
         },
       },
