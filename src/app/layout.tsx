87b5dc8dfd83ad80d85dc478743b2a0c9f2c858a
Minrak Son <dungguk@naver.com>
2025-05-24 23:53:25 +0900

    fix: rootLayout favicon 경로 수정
    - favicon.ico를 매 페이지에서 상대 경로로 참조해 500 에러가 나던 문제 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "락 블로그",
   description: "개발자 손민락의 블로그입니다.",
-  icons: "favicon.ico",
+  icons: "/favicon.ico",
 };
 
 export default function RootLayout({
